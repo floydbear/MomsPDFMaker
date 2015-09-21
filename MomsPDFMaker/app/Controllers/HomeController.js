32a2245fa4ac89d6c33c3b145faa79fa86e175d3
unknown <kfloyd@KiFloyd-m3800.credera.com>
2015-09-21 16:23:07 -0500

    Got routing working

--- MomsPDFMaker/app/Controllers/HomeController.js
+++ MomsPDFMaker/app/Controllers/HomeController.js
@@ -1,5 +1,5 @@
 ﻿(function () {
-    var pdfApp = angular.module('pdfApp', []);
+    var pdfApp = angular.module('pdfApp');
 
     var homeController = pdfApp.controller('HomeController', function () {
         console.log("Home!");
--- MomsPDFMaker/app/app.js
+++ MomsPDFMaker/app/app.js
@@ -2,11 +2,13 @@
     var pdfApp = angular.module('pdfApp', ['ngRoute']);
 
     pdfApp.config(['$routeProvider', function ($routeProvider) {
-        console.log("in config");
+        
         $routeProvider
             .when('/', {
                 templateUrl: '/app/Views/Home.html',
                 controller: 'HomeController'
+            }).otherwise({
+                redirectTo: '/'
             });
     }]);
 
